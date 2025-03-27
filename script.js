e3a56a1c1f7c8e7ff72fac98567d6bb79ee3e537
miroc_whyixd_desktop <miroc@whyicxd.com>
2025-03-27 16:46:47 +0800

    add home button

--- script.js
+++ script.js
@@ -4,61 +4,38 @@ const connectButton = document.getElementById("connectButton");
 const disconnectButton = document.getElementById("disconnectButton");
 const modeSelect = document.getElementById("modeSelect");
 const saveToEepromButton = document.getElementById("saveToEepromButton");
-const idInput = document.getElementById("idInput");
-const setIdButton = document.getElementById("setIdButton");
+const idInput = document.getElementById("flowerSerial");
+const flowerIndex = document.getElementById("flowerIndex");
+const saveConfigButton = document.getElementById("saveConfigButton");
 const openButton = document.getElementById("openButton");
 const closeButton = document.getElementById("closeButton");
+const homeButton = document.getElementById("homeButton");
 const messages = document.getElementById("messages");
+const sensorStateContainer = document.getElementById("sensorStateContainer");
 
 let device;
 let server;
 let modeReadcharacteristic;
 let modeSelectcharacteristic;
+let openClosecharacteristic;
+let configReadcharacteristic;
+let configSavecharacteristic;
+let sensorReadcharacteristic;
 
 const MAIN_SERVICE_UUID = "19b10000-e8f2-537e-4f6c-d104768a1214"; // Replace with your service UUID
 const MODE_READ_CHARACTERISTIC_UUID = "19b10001-e8f2-537e-4f6c-d104768a1214"; // Replace with your characteristic UUID
 const MODE_SELECT_CHARACTERISTIC_UUID = "19b10002-e8f2-537e-4f6c-d104768a1214"; // Replace with your characteristic UUID
 const EEPROM_SAVE_CHARACTERISTIC_UUID = "19b10003-e8f2-537e-4f6c-d104768a1214"; // Add this line for EEPROM save characteristic
+const OPEN_CLOSE_CHARACTERISTIC_UUID = "19b10004-e8f2-537e-4f6c-d104768a1214"; // Add this line for open/close characteristic
+
+const CONFIG_SERVICE_UUID = "1f18bde0-ac02-4c37-8bf4-50dc2d9ac04f";
+const CONFIG_READ_CHARACTERISTIC_UUID = "1f18bde1-ac02-4c37-8bf4-50dc2d9ac04f"; // New UUID for flower index
+const CONFIG_SAVE_CHARACTERISTIC_UUID = "1f18bde2-ac02-4c37-8bf4-50dc2d9ac04f"; // New UUID for flower index
+const SENSOR_READ_CHARACTERISTIC_UUID = "1f18bde3-ac02-4c37-8bf4-50dc2d9ac04f";
+
 let bleServices;
-
-// Check browser compatibility at the beginning
-document.addEventListener("DOMContentLoaded", function () {
-  checkBrowserCompatibility();
-});
-
-// function checkBrowserCompatibility() {
-//   if (!navigator.bluetooth) {
-//     var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-//     // Check if device is iOS
-//     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-//       // Check if Bluefy is installed using URL scheme
-//       // Bluefy is not installed, offer to download
-//       var confirmDownload = confirm(
-//         "This feature requires the Bluefy browser with Web Bluetooth support. Would you like to download it from the App Store?"
-//       );
-//       if (confirmDownload) {
-//         window.location.href =
-//           "https://apps.apple.com/us/app/bluefy-web-ble-browser/id1492822055";
-//       }
-//       // Bluefy is installed, offer to open in Bluefy
-//       // var openInBluefy = confirm(
-//       //   "Would you like to open this page in Bluefy browser for Web Bluetooth support?"
-//       // );
-//       // if (openInBluefy) {
-//       //   // Open current page in Bluefy
-//       //   const currentUrl = window.location.href;
-//       //   window.location.href = `bluefy://${currentUrl}`;
-//       // }
-//     }
-//   } else {
-//     // Not iOS but doesn't support Web Bluetooth
-//     alert(
-//       "Your browser doesn't support Web Bluetooth. Please use a compatible browser."
-//     );
-//   }
-// }
-
+let configService;
+let readSensor;
 scanButton.addEventListener("click", async () => {
   try {
     const filterName =
@@ -70,7 +47,8 @@ scanButton.addEventListener("click", async () => {
       optionalServices: [
         "generic_access",
         "generic_attribute",
-        "19b10000-e8f2-537e-4f6c-d104768a1214", // Replace with your custom service UUID
+        MAIN_SERVICE_UUID, // Replace with your custom service UUID
+        CONFIG_SERVICE_UUID,
         // Add more custom service UUIDs as needed
       ],
     });
@@ -107,6 +85,8 @@ connectButton.addEventListener("click", async () => {
 
     // Read mode and update select
     await readModeAndUpdateSelect();
+    await readFlowerConfigAndUpdate();
+    // await readSensorAndUpdate();
   } catch (error) {
     logMessage(`Error connecting: ${error}`, "error");
   }
@@ -121,25 +101,70 @@ disconnectButton.addEventListener("click", () => {
   if (device.gatt.connected) {
     device.gatt.disconnect();
     logMessage("Disconnected.");
+    clearInterval(readSensor);
+    readSensor = null;
   } else {
     logMessage("Device already disconnected.");
   }
 });
 
-setIdButton.addEventListener("click", async () => {
+saveConfigButton.addEventListener("click", async () => {
   const id = idInput.value;
-  logMessage(`Setting ID: ${id}`);
-  // Implement the logic to send the ID to the device
+  const index = flowerIndex.value ? parseInt(flowerIndex.value) : 0;
+
+  if (!id) {
+    logMessage("Please enter a valid ID.", "error");
+    return;
+  }
+
+  try {
+    await writeConfig(id, index);
+    logMessage(`Config saved: Serial=${id}, Index=${index}`);
+  } catch (error) {
+    logMessage(`Error saving config: ${error}`, "error");
+  }
 });
 
-openButton.addEventListener("click", () => {
+openButton.addEventListener("click", async () => {
   logMessage("Opening...");
   // Implement the open function logic
+  try {
+    const service = await server.getPrimaryService(MAIN_SERVICE_UUID);
+    openClosecharacteristic = await service.getCharacteristic(
+      OPEN_CLOSE_CHARACTERISTIC_UUID
+    );
+    await writeOpenClose(1);
+  } catch (error) {
+    logMessage(`Error writing mode: ${error}`, "error");
+  }
 });
 
-closeButton.addEventListener("click", () => {
+closeButton.addEventListener("click", async () => {
   logMessage("Closing...");
   // Implement the close function logic
+  try {
+    const service = await server.getPrimaryService(MAIN_SERVICE_UUID);
+    openClosecharacteristic = await service.getCharacteristic(
+      OPEN_CLOSE_CHARACTERISTIC_UUID
+    );
+    await writeOpenClose(0);
+  } catch (error) {
+    logMessage(`Error writing mode: ${error}`, "error");
+  }
+});
+
+// Add home button event listener
+homeButton.addEventListener("click", async () => {
+  logMessage("Homing...");
+  try {
+    const service = await server.getPrimaryService(MAIN_SERVICE_UUID);
+    openClosecharacteristic = await service.getCharacteristic(
+      OPEN_CLOSE_CHARACTERISTIC_UUID
+    );
+    await writeOpenClose(2); // Using value 2 for home command
+  } catch (error) {
+    logMessage(`Error sending home command: ${error}`, "error");
+  }
 });
 
 async function readModeAndUpdateSelect() {
@@ -173,6 +198,53 @@ async function readModeAndUpdateSelect() {
   }
 }
 
+async function readFlowerConfigAndUpdate() {
+  try {
+    const service = await server.getPrimaryService(CONFIG_SERVICE_UUID);
+    configReadcharacteristic = await service.getCharacteristic(
+      CONFIG_READ_CHARACTERISTIC_UUID
+    );
+    const value = await configReadcharacteristic.readValue();
+    let serial = value.getInt8(0); // Assuming mode is a single byte
+    let index = value.getInt8(4); // Assuming mode is a single byte
+    console.log("config:", serial, index);
+    idInput.value = serial;
+    flowerIndex.value = index;
+  } catch (error) {
+    logMessage(`Error reading mode: ${error}`, "error");
+  }
+}
+
+// async function readSensorAndUpdate() {
+//   if (!server) {
+//     // logMessage("Please connect to a device first.");
+//     return;
+//   }
+//   try {
+//     const service = await server.getPrimaryService(CONFIG_SERVICE_UUID);
+//     sensorReadcharacteristic = await service.getCharacteristic(
+//       SENSOR_READ_CHARACTERISTIC_UUID
+//     );
+//     const value = await sensorReadcharacteristic.readValue();
+//     let result = value;
+//     console.log("sensor:", result);
+//   } catch (error) {
+//     logMessage(`Error reading mode: ${error}`, "error");
+//   }
+// }
+async function writeConfig(serial, index) {
+  try {
+    const service = await server.getPrimaryService(CONFIG_SERVICE_UUID);
+    configSavecharacteristic = await service.getCharacteristic(
+      CONFIG_SAVE_CHARACTERISTIC_UUID
+    );
+    const config = new Uint8Array([serial, index]);
+    await configSavecharacteristic.writeValue(config);
+    logMessage(`Config set to ${config}`);
+  } catch (error) {
+    logMessage(`Error writing mode: ${error}`, "error");
+  }
+}
 async function writeMode(value) {
   try {
     const mode = new Uint8Array([value]);
@@ -180,6 +252,16 @@ async function writeMode(value) {
     logMessage(`Mode set to ${value}`);
   } catch (error) {
     logMessage(`Error writing mode: ${error}`, "error");
+  }
+}
+
+async function writeOpenClose(value) {
+  try {
+    const mode = new Uint8Array([value]);
+    await openClosecharacteristic.writeValue(mode);
+    logMessage(`Open Close ${value}`);
+  } catch (error) {
+    logMessage(`Error open close: ${error}`, "error");
   }
 }
 
